34393a49b6863e57a14b96a68de3a6fda5fff0c1
Luiz Sol <luizedusol@gmail.com>
2018-04-06 15:04:19 -0300

    Fim do experimento 4

--- exp04/docs/readme.md
+++ exp04/docs/readme.md
@@ -498,3 +498,231 @@ end arch;
 ## 4) Modificação de Testbench para `fsm_main`
 
 ***Perguntas***: Conseguiu verificar a passagem pelo novo estado IDLE e as suas transições? Seguindo as recomendações, a simulação mostrou o comportamento esperado do testbench como descrito na apostila de conceitos?
+
+Resultado:
+Estrutura da pasta:
+```
+comb
+|-- clock_generator.vhd
+|-- fsm_main_2.vhd
+|-- script_compilacao.tcl
+|-- stimuli_seq_2.vhd
+`-- testbench_seq_2.vhd
+```
+
+Script de compilação:
+```tcl
+# Saindo de uma possível simulação anterior
+quit -sim
+# Definindo variáveis da compilação e simulação
+set diretorio "X:/projects/cis2/exp04/seq_2"
+
+set arquivo1 "fsm_main_2.vhd"
+set componente1 "fsm_main"
+
+set arquivo2 "clock_generator.vhd"
+set componente2 "clock_generator"
+
+set arquivo3 "stimuli_seq_2.vhd"
+set componente3 "stimuli_seq_2"
+
+set arquivo4 "testbench_seq_2.vhd"
+set componente4 "tb_fsm_main_seq_2"
+
+set duration [expr {350 * 10}]
+# Definindo o diretório do projeto
+cd $diretorio
+# Criando as libraries do projeto
+vlib work
+vmap work work
+# Compilando o projeto
+vcom -reportprogress 300 -work work $diretorio/$arquivo1
+vcom -reportprogress 300 -work work $diretorio/$arquivo2
+vcom -reportprogress 300 -work work $diretorio/$arquivo3
+vcom -reportprogress 300 -work work $diretorio/$arquivo4
+# Modificando as opções de visualização do projeto
+vsim -gui -voptargs=+acc work.$componente4
+view wave
+# Configurando os sinais a serem apresentados
+add wave -position insertpoint sim:/$componente4/*
+add wave -position insertpoint sim:/$componente4/dut/STATE
+add wave -position insertpoint sim:/$componente4/dut/NEXT_STATE
+run $duration ns
+```
+
+Novo arquivo de estímulos (`stimuli_seq_2.vhd`):
+```vhdl
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity stimuli_seq_2 is
+    generic
+    (
+    WIDTH:         NATURAL := 8;
+    CLK_PERIOD      : TIME  :=  10ns
+    );
+
+    port
+    (
+    cnt_rdy     : out STD_LOGIC;                --from counter to fsms
+    cmp_food_flag   : out STD_LOGIC;            --from datapath/comparator module
+    fsm_i_done      : out STD_LOGIC;            --to fsm_init
+    fsm_f_done      : out STD_LOGIC;            --to fsm_food
+    fsm_s_done      : out STD_LOGIC;            --to fsm_step
+    fsm_s_game_over : out STD_LOGIC ;           --to fsm_step
+    clk : out STD_LOGIC;
+    rst : out STD_LOGIC;
+    fsm_i_start     : in STD_LOGIC;     --to fsm_init
+    fsm_f_start     : in STD_LOGIC;     --to fsm_food
+    fsm_s_start     : in STD_LOGIC          --to fsm_start
+    );
+
+end stimuli_seq_2 ;
+
+architecture test of stimuli_seq_2  is
+    signal clk_s : STD_LOGIC;
+
+    component clock_generator
+        generic (
+        CLK_PERIOD      : TIME  := 10ns
+        );
+
+        port (
+        clk     : out STD_LOGIC
+        );
+
+end component ;
+
+begin
+
+
+    clk <=clk_s;
+
+    clock: clock_generator
+        port map
+        (
+        clk     => clk_s
+        );
+
+
+    sim : process
+
+procedure check_fsm(fsm_i_done_value, fsm_f_done_value, fsm_s_done_value, fsm_s_game_over_value, cmp_food_flag_value, cnt_rdy_value : in STD_LOGIC) is
+        begin
+            -- Assign values to estimuli_module´s outputs.
+            fsm_i_done <= fsm_i_done_value;
+            fsm_f_done <= fsm_f_done_value;
+            fsm_s_done <= fsm_s_done_value;
+            fsm_s_game_over <= fsm_s_game_over_value;
+            cmp_food_flag <= cmp_food_flag_value;
+            cnt_rdy <= cnt_rdy_value;
+
+            wait until rising_edge (clk_s);
+            -- Events at the rising edge of next clock cycle
+            end procedure check_fsm;
+
+        procedure reset_activate is    -- reset activation procedure
+        begin
+            wait until falling_edge(CLK_s);
+            rst <= '1';
+            wait for CLK_PERIOD;
+            rst <= '0';
+        end procedure reset_activate;
+
+
+    begin
+        -- Apply test vectors
+
+        check_fsm('0', '0', '0', '0', '0', '0');    -- analyze state transitions
+        reset_activate;
+
+    -- **initialization state** protocol
+
+        while (fsm_i_start /= '1')
+            loop
+            check_fsm('0', '0', '0', '0', '0', '0');    -- guaranteeing fsm_i_done='0'
+        end loop;
+
+        -- 25 ns
+
+        -- emulate some action
+
+        wait for 50*CLK_PERIOD;
+
+        -- 525 ns
+
+        -- forcing fsm_i_done='1'
+
+        check_fsm('1', '0', '0', '0', '0', '0');     -- forcing fsm_i_done='1' and fsm_f_done='0'
+
+        wait for CLK_PERIOD;
+
+        -- 540 ns
+
+    -- **food state** protocol
+
+        while (fsm_f_start /= '1')
+            loop
+            check_fsm('1', '0', '0', '0', '0', '0');  -- maintaining fsm_i_done='1' and fsm_f_done='0'
+        end loop;
+
+        -- FOOD_ACTIVATION
+
+        -- 540 ns
+
+        -- emulate some action
+
+        wait for 50*CLK_PERIOD;
+
+        check_fsm('0', '1', '0', '0', '0', '0');    -- forcing fsm_f_done='1' and fsm_s_done='0'
+
+        wait for CLK_PERIOD;
+
+        -- 1050 ns
+
+    -- **step state** protocol
+
+
+        while (fsm_f_start /= '0')
+            loop
+            check_fsm('0', '1', '0', '0', '0', '0');  -- maintaining fsm_f_done='1' and fsm_s_done='0'
+        end loop;
+
+        -- IDLE
+
+        check_fsm('0', '0', '0', '0', '0', '0');
+
+        wait for 50*CLK_PERIOD;
+
+        while (fsm_s_start /= '1')
+            loop
+            check_fsm('0', '0', '0', '0', '0', '1');  -- maintaining fsm_f_done='1' and fsm_s_done='0'
+        end loop;
+
+        -- STEP ACTIVATION
+
+        wait for 50*CLK_PERIOD;
+
+        while (fsm_s_start /= '0')
+            loop
+            check_fsm('0', '0', '0', '1', '0', '0');
+        end loop;
+
+        wait;
+    end process sim;
+end architecture test;
+```
+
+Resultado da simulação:
+
+![Resultado da primeira simulação](img/wave_seq_2_01.bmp)
+
+> Conseguiu verificar a passagem pelo novo estado IDLE e as suas transições?
+
+Sim.
+
+
+> Seguindo as recomendações, a simulação mostrou o comportamento esperado do testbench como descrito na apostila de conceitos?
+
+Sim.
--- exp04/seq_2/stimuli_seq_2.vhd
+++ exp04/seq_2/stimuli_seq_2.vhd
@@ -3,183 +3,158 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity stimuli_seq_2 is
-	generic
-	(
-	WIDTH:         NATURAL := 8;
-	CLK_PERIOD		: TIME	:=  10ns
-	);
+    generic
+    (
+    WIDTH:         NATURAL := 8;
+    CLK_PERIOD      : TIME  :=  10ns
+    );
 
-	port
-	(
-	cnt_rdy		: out STD_LOGIC;				--from counter to fsms
-	cmp_food_flag	: out STD_LOGIC;			--from datapath/comparator module
-	fsm_i_done		: out STD_LOGIC;			--to fsm_init
-	fsm_f_done		: out STD_LOGIC;			--to fsm_food
-	fsm_s_done		: out STD_LOGIC;			--to fsm_step
-	fsm_s_game_over	: out STD_LOGIC	;			--to fsm_step
-	clk : out STD_LOGIC;
-	rst	: out STD_LOGIC;
-	fsm_i_start		: in STD_LOGIC;		--to fsm_init
-	fsm_f_start		: in STD_LOGIC;		--to fsm_food
-	fsm_s_start		: in STD_LOGIC			--to fsm_start
-	);
+    port
+    (
+    cnt_rdy     : out STD_LOGIC;                --from counter to fsms
+    cmp_food_flag   : out STD_LOGIC;            --from datapath/comparator module
+    fsm_i_done      : out STD_LOGIC;            --to fsm_init
+    fsm_f_done      : out STD_LOGIC;            --to fsm_food
+    fsm_s_done      : out STD_LOGIC;            --to fsm_step
+    fsm_s_game_over : out STD_LOGIC ;           --to fsm_step
+    clk : out STD_LOGIC;
+    rst : out STD_LOGIC;
+    fsm_i_start     : in STD_LOGIC;     --to fsm_init
+    fsm_f_start     : in STD_LOGIC;     --to fsm_food
+    fsm_s_start     : in STD_LOGIC          --to fsm_start
+    );
 
 end stimuli_seq_2 ;
 
 architecture test of stimuli_seq_2  is
-	signal clk_s : STD_LOGIC;
+    signal clk_s : STD_LOGIC;
 
-	component clock_generator
-		generic (
-		CLK_PERIOD		: TIME	:= 10ns
-		);
+    component clock_generator
+        generic (
+        CLK_PERIOD      : TIME  := 10ns
+        );
 
-		port (
-		clk		: out STD_LOGIC
-		);
+        port (
+        clk     : out STD_LOGIC
+        );
 
 end component ;
 
 begin
 
 
-	clk <=clk_s;
+    clk <=clk_s;
 
-	clock: clock_generator
-		port map
-		(
-		clk		=> clk_s
-		);
-
-
-	sim : process
-
-procedure check_fsm(fsm_i_done_value, fsm_f_done_value, fsm_s_done_value, fsm_s_game_over_value, cmp_food_flag_value	: in STD_LOGIC)	is
-		begin
-			-- Assign values to estimuli_module´s outputs.
-			fsm_i_done <= fsm_i_done_value;
-			fsm_f_done <= fsm_f_done_value;
-			fsm_s_done <= fsm_s_done_value;
-			fsm_s_game_over <= fsm_s_game_over_value;
-			cmp_food_flag <= cmp_food_flag_value;
-
-			wait until rising_edge (clk_s);
-			-- Events at the rising edge of next clock cycle
-			end procedure check_fsm;
-
-		procedure reset_activate is    -- reset activation procedure
-		begin
-			wait until falling_edge(CLK_s);
-			rst <= '1';
-			wait for CLK_PERIOD;
-			rst <= '0';
-		end procedure reset_activate;
+    clock: clock_generator
+        port map
+        (
+        clk     => clk_s
+        );
 
 
-	begin
-		-- Apply test vectors
+    sim : process
 
-		check_fsm('0','0','0','0','0');    -- analyze state transitions
-		reset_activate;
+procedure check_fsm(fsm_i_done_value, fsm_f_done_value, fsm_s_done_value, fsm_s_game_over_value, cmp_food_flag_value, cnt_rdy_value : in STD_LOGIC) is
+        begin
+            -- Assign values to estimuli_module´s outputs.
+            fsm_i_done <= fsm_i_done_value;
+            fsm_f_done <= fsm_f_done_value;
+            fsm_s_done <= fsm_s_done_value;
+            fsm_s_game_over <= fsm_s_game_over_value;
+            cmp_food_flag <= cmp_food_flag_value;
+            cnt_rdy <= cnt_rdy_value;
 
-	-- **initialization state** protocol
+            wait until rising_edge (clk_s);
+            -- Events at the rising edge of next clock cycle
+            end procedure check_fsm;
 
-		while (fsm_i_start /= '1')
-			loop
-			check_fsm('0','0','0','0','0');    -- guaranteeing fsm_i_done='0'
-		end loop;
-
-		-- emulate some action
-
-		wait for 50*CLK_PERIOD;
-
-		-- forcing fsm_i_done='1'
-
-		check_fsm('1','0','0','0','0');     -- forcing fsm_i_done='1' and fsm_f_done='0'
-
-		wait for CLK_PERIOD;
-
-	-- **food state** protocol
-
-		while (fsm_f_start /= '1')
-			loop
-			check_fsm('1','0','0','0','0');  -- maintaining fsm_i_done='1' and fsm_f_done='0'
-		end loop;
-
-		-- emulate some action
-
-		wait for 50*CLK_PERIOD;
-
-		check_fsm('0','1','0','0','0');    -- forcing fsm_f_done='1' and fsm_s_done='0'
-
-		wait for CLK_PERIOD;
-
-	-- **step state** protocol
-
-		while (fsm_s_start /= '1')
-			loop
-			check_fsm('0','1','0','0','0');  -- maintaining fsm_f_done='1' and fsm_s_done='0'
-		end loop;
-
-		-- emulate some action
-
-		wait for 50*CLK_PERIOD;
-
-		check_fsm('0','0','0','0','1');    -- forcing fsm_f_done='0', fsm_s_game_over='0' and cmp_food_flag='1', making fsm_main to return to food state
-
-		wait for CLK_PERIOD;
-
-	-- **food state 2nd time** protocol
-
-		while (fsm_f_start /= '1')
-			loop
-		check_fsm('0','0','0','0','1');    -- maintaining fsm_f_done='0', fsm_s_game_over='0' and cmp_food_flag='1', making fsm_main to return to food state
-		end loop;
-
-		-- emulate some action
-
-		wait for 50*CLK_PERIOD;
-
-		check_fsm('0','1','0','0','0');    -- forcing fsm_f_done='1' and fsm_s_done='0'
-
-		wait for CLK_PERIOD;
-
-	-- **step state 2nd time** protocol
-
-		while (fsm_s_start /= '1')
-			loop
-			check_fsm('0','1','0','0','0');  -- maintaining fsm_f_done='1' and fsm_s_done='0'
-		end loop;
-
-		-- emulate some action
-
-		wait for 50*CLK_PERIOD;
-
-		check_fsm('0','0','0','0','0');    -- forcing fsm_f_done='0', fsm_s_game_over='0' and cmp_food_flag='0', making fsm_main to stay in step state
-
-		wait for CLK_PERIOD;
-
-	-- **step state again** protocol
-
-		while (fsm_s_start /= '1')
-			loop
-			check_fsm('0','0','0','0','0');  -- maintaining fsm_f_done='0', fsm_s_game_over='0' and cmp_food_flag='0'
-		end loop;
-
-		-- emulate some action
-
-		wait for 50*CLK_PERIOD;
-
-		check_fsm('0','0','0','1','0');    -- forcing fsm_s_game_over='1', making fsm_main to go to state game over
+        procedure reset_activate is    -- reset activation procedure
+        begin
+            wait until falling_edge(CLK_s);
+            rst <= '1';
+            wait for CLK_PERIOD;
+            rst <= '0';
+        end procedure reset_activate;
 
 
-		-- forcing signal: any force on fsm_i, fsm_f, fsm_s, game_over, cmp_food should be useless- fsm must stay in game over.
+    begin
+        -- Apply test vectors
 
-		check_fsm('1','0','1','0','0');
-		check_fsm('1','0','1','0','0');
-		check_fsm('1','0','1','0','0');
-		check_fsm('0','1','1','0','0');
+        check_fsm('0', '0', '0', '0', '0', '0');    -- analyze state transitions
+        reset_activate;
 
-		wait;
-	end process sim;
+    -- **initialization state** protocol
+
+        while (fsm_i_start /= '1')
+            loop
+            check_fsm('0', '0', '0', '0', '0', '0');    -- guaranteeing fsm_i_done='0'
+        end loop;
+
+        -- 25 ns
+
+        -- emulate some action
+
+        wait for 50*CLK_PERIOD;
+
+        -- 525 ns
+
+        -- forcing fsm_i_done='1'
+
+        check_fsm('1', '0', '0', '0', '0', '0');     -- forcing fsm_i_done='1' and fsm_f_done='0'
+
+        wait for CLK_PERIOD;
+
+        -- 540 ns
+
+    -- **food state** protocol
+
+        while (fsm_f_start /= '1')
+            loop
+            check_fsm('1', '0', '0', '0', '0', '0');  -- maintaining fsm_i_done='1' and fsm_f_done='0'
+        end loop;
+
+        -- FOOD_ACTIVATION
+
+        -- 540 ns
+
+        -- emulate some action
+
+        wait for 50*CLK_PERIOD;
+
+        check_fsm('0', '1', '0', '0', '0', '0');    -- forcing fsm_f_done='1' and fsm_s_done='0'
+
+        wait for CLK_PERIOD;
+
+        -- 1050 ns
+
+    -- **step state** protocol
+
+
+        while (fsm_f_start /= '0')
+            loop
+            check_fsm('0', '1', '0', '0', '0', '0');  -- maintaining fsm_f_done='1' and fsm_s_done='0'
+        end loop;
+
+        -- IDLE
+
+        check_fsm('0', '0', '0', '0', '0', '0');
+
+        wait for 50*CLK_PERIOD;
+
+        while (fsm_s_start /= '1')
+            loop
+            check_fsm('0', '0', '0', '0', '0', '1');  -- maintaining fsm_f_done='1' and fsm_s_done='0'
+        end loop;
+
+        -- STEP ACTIVATION
+
+        wait for 50*CLK_PERIOD;
+
+        while (fsm_s_start /= '0')
+            loop
+            check_fsm('0', '0', '0', '1', '0', '0');
+        end loop;
+
+        wait;
+    end process sim;
 end architecture test;

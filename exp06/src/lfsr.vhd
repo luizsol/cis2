81e30d1e7ac112903e6b354fa5bdf2d2d4b5d91e
Luiz Sol <luizedusol@gmail.com>
2018-05-13 20:43:59 -0300

    Implementa uma possível versão funcional da lfsr

--- exp06/src/lfsr.vhd
+++ exp06/src/lfsr.vhd
@@ -2,19 +2,19 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.NUMERIC_STD.all;
 
-entity lsfr is
+entity lfsr is
     generic(
-        WIDTH: natural := 12,
-        POLIN  : STD_LOGIC_VECTOR (12 downto 0) := "1110011011011"
+        WIDTH: natural := 12;
+        POL  : STD_LOGIC_VECTOR (12 downto 0) := "1110011011011"
     );
     port (
         clk: in  STD_LOGIC;
         res: in  STD_LOGIC;
-        o  : in  STD_LOGIC_VECTOR(WIDTH - 1 downto 0)
+        o  : out STD_LOGIC_VECTOR (7 downto 0)
     );
-end lsfr;
+end lfsr;
 
-architecture arch of lsfr is
+architecture arch of lfsr is
     COMPONENT xor2
         PORT (
             x, y: IN STD_LOGIC;
@@ -24,6 +24,7 @@ architecture arch of lsfr is
 
     signal prev_state: STD_LOGIC_VECTOR(WIDTH - 1 downto 0);
     signal next_state: STD_LOGIC_VECTOR(WIDTH - 1 downto 0);
+    signal or_signals: STD_LOGIC_VECTOR(WIDTH - 1 downto 0);
 
 begin
     seq : process(clk)
@@ -35,19 +36,19 @@ begin
 
     layout: for I in 1 to (WIDTH - 1) generate
         zeros: if POL(I) = '0' generate
-            next_state(I) <= state(I - 1) or rst;
+            next_state(I) <= prev_state(I - 1) or res;
         end generate zeros;
         ones: if POL(I) = '1' generate
             generated_xor2: xor2 port map (
-            state(I - 1),
-            state(WIDTH - 1),
-            Q(I) -- where to put or?
-         );
+                prev_state(I - 1),
+                prev_state(WIDTH - 1),
+                or_signals(I)
+            );
 
-            next_state(I) <=(state(I - 1) xor state(WIDTH - 1) or rst;
+            next_state(I) <= or_signals(I) or res;
         end generate ones;
     end generate layout;
-    next_state(0) <= state(WIDTH - 1) or rst;
+    next_state(0) <= prev_state(WIDTH - 1) or res;
 
-    o <= '0' & state(WIDTH - 1 downto WIDTH - 3) & '0' & state(2 downto 0);
+    o <= '0' & prev_state(WIDTH - 1 downto WIDTH - 3) & '0' & prev_state(2 downto 0);
 end arch;
